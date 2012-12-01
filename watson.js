ab20b4b440c94a4f5946023c0c2a75382cf34ce5
Neil Gupta <neil@tabuleapp.com>
2012-11-30 22:04:11 -0600

    added demo

--- watson.js
+++ watson.js
@@ -5,9 +5,28 @@
 
 var Watson = (function() {
 	var helpers = {
+		// Validates the parsed times returned by Sherlock
+		validate: function(Sherlocked) {
+			if (!Sherlocked.startDate)
+				return 'Sorry, you\'ve entered an invalid date :(';
 
-		// Place your helper functions here...
+			if (Sherlocked.endDate && Sherlocked.startDate > Sherlocked.endDate)
+				return 'Sorry, the end time can\'t be before the start time.';
 
+			return true;
+		},
+
+		// Parse the input string to try to figure out which calendar the user wants.
+		parseCalendar: function(str, Sherlocked) {
+
+			// Simple example of searching for CS 104 calendar and removing it from input string
+			if (str.indexOf('CS 104') !== -1) {
+				Sherlocked.calendar = 'CS 104';
+				str = str.replace('CS 104', '');
+			}
+
+			return [str, Sherlocked];
+		},
 	};
 
 	return {
@@ -16,20 +35,14 @@ var Watson = (function() {
 		 * an array with the modified input string at position 0 and a new Sherlocked object at position 1
 		*/
 		preprocess: function(str) {
-			var Sherlocked = {};
-
-			// Manipulate str and Sherlocked here...
-
-			return [str, Sherlocked];
+			return helpers.parseCalendar(str, {});
 		},
 
 		/* 
 		 * Takes a Sherlocked object, and returns that Sherlocked object with any desired modifications.
 		*/
 		postprocess: function(Sherlocked) {
-
-			// Manipulate Sherlocked here...
-
+			Sherlocked.validated = helpers.validate(Sherlocked);
 			return Sherlocked;
 		}
 	};
